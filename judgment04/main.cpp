4849e7241b220d16acc85c3449a90f03fdc14ae1
s20181106117 <1843132025@qq.com>
2019-06-20 12:01:48 +0800

    four

--- judgment04/main.cpp
+++ judgment04/main.cpp
@@ -7,9 +7,38 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int i,j,n,a[10],pj=0,h=0;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(j=0;j<n;j++)
+    {
+        for(i=0;i<n;i++)
+        {
+            if(a[i]>a[i+1])
+            {
+                a[n]=a[i];
+                a[i]=a[i+1];
+                a[i+1]=a[n];
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    for(i=1;i<n-1;i++)
+    {
+        h=a[i]+h;
+        pj=h/(n-2);
+    }
+    cout<<pj<<endl;
     return 0;
 }
+
+
