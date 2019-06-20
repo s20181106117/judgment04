51ac43f203fae7fb776d7573997d68fd61355666
s20181106117 <1843132025@qq.com>
2019-06-20 12:01:14 +0800

    Initial Commit

---
+++ judgment04/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  judgment04
+//
+//  Created by s20181106117 on 2019/6/20.
+//  Copyright © 2019 s20181106117. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
