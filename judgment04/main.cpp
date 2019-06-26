182ca8751985d9ebd1877f9d7843d432bb04bf29
s20181106117 <1843132025@qq.com>
2019-06-26 19:39:16 +0800

    Update main.cpp

--- judgment04/main.cpp
+++ judgment04/main.cpp
@@ -9,7 +9,30 @@
 #include <iostream>
 using namespace std;
 int main()
-{
+{ int a,b;
+    ifstream in1("/Users/s20181106117/Desktop/caipan.txt ");
+    ifstream in2("/Users/s20181106117/Desktop/caipan-2.txt ");
+    ofstream out("/Users/s20181106117/Desktop/chengji.txt ");
+    if(in1.is_open())
+    {
+        cout<<"file ok"<<endl;
+        in1>>a;
+        cout<<a<<" "<<endl;
+        in1.close();
+    }
+    if(in2.is_open())
+    {
+        cout<<"file ok"<<endl;
+        in2>>b;
+        cout<<b<<" "<<endl;
+        in2.close();
+    }
+    if(out.is_open())
+    {
+        out<<"c="<<a+b<<endl;
+        cout<<"c="<<a+b<<endl;
+        out.close();
+    }
     int i,j,n,a[10],pj=0,h=0;
     cin>>n;
     for(i=0;i<n;i++)
